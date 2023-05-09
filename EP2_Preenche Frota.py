d24c0088d7a5d35c50eaaace843dd8a6bd33a7b8
Vinicius Dib <viniciusdib15@gmail.com>
2023-05-09 14:24:15 -0400

    Preenche Frota - Vinicius

---
+++ EP2_Preenche Frota.py
@@ -0,0 +1,31 @@
+import random
+
+def define_posicoes (linha, coluna, orientacao, tamanho):
+    lista = []
+
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            posicao = []
+            posicao.append(linha+i)
+            posicao.append(coluna)
+            lista.append(posicao)
+
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            posicao = []
+            posicao.append(linha)
+            posicao.append(coluna+i)
+            lista.append(posicao)
+
+
+    return lista
+
+##### EP2 - Preenche Frota #######
+
+def preenche_frota (frota, nome_navio, linha, coluna, orientacao, tamanho):
+    if nome_navio in frota:
+        frota[nome_navio].append(define_posicoes(linha, coluna, orientacao, tamanho))
+    else:
+        frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+    return frota
+
